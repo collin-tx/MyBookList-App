2b1c3c2212c42249f1506c308a49a7ed78978b00
Collin Miller Smith <44204853+collin-tx@users.noreply.github.com>
2019-02-19 15:36:59 -0600

    Update app.js
    display, add books to UI

--- app.js
+++ app.js
@@ -56,3 +56,19 @@ class UI {
 
 
 // Events: Display, Add, Remove Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  // Prevent actual submit
+  e.preventDefault();
+  
+  //Get form values
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const isbn = document.querySelector("#isbn").value;
+  
+  // instantiate new book
+  const book = new Book(title, author, isbn);
+
+  // Add book to UI
+  UI.addBookToList(book);
+});
